9f02f2876e1de3a27dcd173d4876c3b8e8e2fced
Matxpcode <mateo.rubiuz@gmail.com>
2026-08-31 12:49:29 -0500

    feat:add project 2

---
+++ 02-spring-IoC/springboot-di/src/main/java/com/wreckcode/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.wreckcode.springboot.di.app.springboot_di.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.wreckcode.springboot.di.app.springboot_di.models.Product;
+
+public class ProductRepository {
+    //1.definimos la lista (simula la BD)
+    private List<Product> data;
+
+    public ProductRepository(){
+        this.data = Arrays.asList(
+            new Product(1L, "Memoria corsair 32", 300L),
+            new Product(2L, "Cpu Intel Core i9", 850L),
+            new Product(3L, "Teclado Ryzen Mini 60%", 180L),
+            new Product(4L, "Motherboard  Gigabyte", 490L)
+        );
+    }
+
+    public List<Product> findAll(){
+        return data;
+    }
+
+    public Product findById(Long id){
+        return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
+    }
+}
---
+++ 02-spring-IoC/springboot-di/src/main/java/com/wreckcode/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.wreckcode.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.wreckcode.springboot.di.app.springboot_di.models.Product;
+import com.wreckcode.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+public class ProductService {
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll(){
+        return repository.findAll().stream().map(p->{   //300
+            Double priceImp = p.getPrice()*1.25d;   //375
+
+            Product newProduct = new Product(p.getId(), p.getName(), priceImp.longValue());
+            return newProduct;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id){
+        return repository.findById(id);
+    }
+}
