b1382414e3eec498240dd8bcdace40c98008518e
Matxpcode <mateo.rubiuz@gmail.com>
2026-09-10 16:09:37 -0500

    feat: add new bean default, imported total method and property

--- 03-IoC-factura/springboot-di-factura/src/main/java/com/wrekcode/curso/springboot/di/factura/springboot_di_factura/AppConfig.java
+++ 03-IoC-factura/springboot-di-factura/src/main/java/com/wrekcode/curso/springboot/di/factura/springboot_di_factura/AppConfig.java
@@ -10,20 +10,37 @@ import org.springframework.context.annotation.PropertySource;
 import com.wrekcode.curso.springboot.di.factura.springboot_di_factura.models.Item;
 import com.wrekcode.curso.springboot.di.factura.springboot_di_factura.models.Product;
 
-@Configuration 
-@PropertySource(value="classpath:data.properties",encoding="UTF-8") 
+@Configuration // indica fabrica de beans
+@PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
-    
-    //Creamos un componente de fabrica
-    @Bean("listadoItemsDeporte")
-    List<Item> itemsInvoice(){
-        Product p1 = new Product("Camara Sony",800);
-        Product p2 = new Product("Bicicleta Goliat",1200);
-        
-        Item item1 = new Item(p1, 10);
-        Item item2 = new Item(p2, 5);
 
-        return Arrays.asList(item1,item2);
-    }
-    //puede haber mas de 1 bean de tipo List<Item> ....
+  // Componente de fabrica - listar items deporte
+  @Bean
+  List<Item> itemsInvoice() { // deja de ser ambiguo a tener datos fijos
+    Product p1 = new Product("Camara Sony", 800);
+    Product p2 = new Product("Bicicleta Goliat", 1200);
+
+    Item item1 = new Item(p1, 10);
+    Item item2 = new Item(p2, 5);
+
+    return Arrays.asList(item1, item2);
+  }
+
+  // puede haber mas de 1 bean de tipo List<Item> ....
+
+  // Componente de fabrica - listar items oficina
+  @Bean("default")
+  List<Item> itemsInvoiceOffice() {
+    Product p1 = new Product("Monitor Asus 24", 700);
+    Product p2 = new Product("Notebook Razer", 2400);
+    Product p3 = new Product("Impresora HP", 800);
+    Product p4 = new Product("Escritorio Oficina", 900);
+
+    Item item1 = new Item(p1, 10);
+    Item item2 = new Item(p2, 20);
+    Item item3 = new Item(p3, 30);
+    Item item4 = new Item(p4, 40);
+
+    return Arrays.asList(item1, item2, item3, item4);
+  }
 }
--- 03-IoC-factura/springboot-di-factura/src/main/java/com/wrekcode/curso/springboot/di/factura/springboot_di_factura/models/Invoice.java
+++ 03-IoC-factura/springboot-di-factura/src/main/java/com/wrekcode/curso/springboot/di/factura/springboot_di_factura/models/Invoice.java
@@ -6,34 +6,48 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component 
+@Component
 public class Invoice {
-    private final Client client;
+  private final Client client;
 
-    private final String description;
- 
-    private final List<Item> items;
+  private final String description;
 
-    //Inyeccion de dependencias
-    Invoice(Client client,@Value("${invoice.description}") String description,@Qualifier("listadoItemsDeporte") List<Item> items) {
-        this.client = client;
-        this.description=description;
-        this.items = items;
-    }
+  private final List<Item> items;
 
-    public Client getClient() {
-        return client;
-    }
+  // Inyeccion de dependencias
+  Invoice(Client client, @Value("${invoice.description.office}") String description,
+      @Qualifier("default") List<Item> items) {
+    this.client = client;
+    this.description = description;
+    this.items = items;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public Client getClient() {
+    return client;
+  }
 
-    public List<Item> getItems() {
-        return items;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public int getTotal(){
-        return 0;
-    }
+  public List<Item> getItems() {
+    return items;
+  }
+
+  public int getTotal() {
+
+    // forma 1: usando foreach
+    /*
+     * int Itotal = 0;
+     * foreachr (Item item : items) {
+     * Itotal += item.getImporte();
+     * }
+     * return Itotal;
+     */
+
+    // forma 2: usando api stream - programacion funcional
+    return items.stream()
+        .map(item -> item.getImporte())
+        .reduce(0, (sum, importe) -> sum + importe);
+  }
 }
